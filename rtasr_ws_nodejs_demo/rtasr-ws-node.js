2a4fe14b4fde5301c4cf0649da2797cf89e2de52
Chi Zhang <chizhang0814@gmail.com>
2020-09-05 00:43:54 -0700

    added metric module and youtube module

--- rtasr_ws_nodejs_demo/rtasr-ws-node.js
+++ rtasr_ws_nodejs_demo/rtasr-ws-node.js
@@ -10,13 +10,42 @@
  */
 const CryptoJS = require('crypto-js')
 const WebSocket = require('ws')
+const AudioRecorder = require('node-audiorecorder');
+const DIRECTORY = 'examples-recordings';
+
+// Options is an optional parameter for the constructor call.
+// If an option is not given the default value, as seen below, will be used.
+const options = {
+  program: `sox`,     // Which program to use, either `arecord`, `rec`, or `sox`.
+  //device: `hw:2,0`,       // Recording device to use, e.g. `hw:1,0`
+ 
+  bits: 16,           // Sample size. (only for `rec` and `sox`)
+  channels: 1,        // Channel count.
+  encoding: `signed-integer`,  // Encoding type. (only for `rec` and `sox`)
+  format: `S16_LE`,   // Encoding type. (only for `arecord`)
+  rate: 16000,        // Sample rate.
+  type: `wav`,        // Format type.
+ 
+  // Following options only available when using `rec` or `sox`.
+  silence: 0,         // Duration of silence in seconds before it stops recording.
+  //thresholdStart: 0.5,  // Silence threshold to start recording.
+  //thresholdStop: 0.5,   // Silence threshold to stop recording.
+  //keepSilence: true   // Keep the silence in the recording.
+};
+// The object has to implement a log and warn function.
+const logger = console;
+ 
+// Create an instance.
+let audioRecorder = new AudioRecorder(options, logger);
+
+//
 var fs = require('fs')
 var log = require('log4node')
-
+const path = require('path');
 // 系统配置
 const config = {
   // 请求地址
-  hostUrl: "wss://rtasr.xfyun.cn/v1/ws",
+  hostUrl: "ws://rtasr.xfyun.cn/v1/ws",
   //在控制台-我的应用-实时语音转写获取
   appid: "5f080cd1",
   //在控制台-我的应用-实时语音转写获取
@@ -36,11 +65,60 @@ ws.on('open', (event) => {
   log.info("websocket connect!")
 })
 
+//build socket connection to TEDMirror module
+
+const net = require('net');
+const socket = new net.Socket();
+const port = 3000;
+const hostname = 'localhost';
+socket.setEncoding('UTF-8');
+
+socket.connect( port,hostname,function(){
+  socket.write('Recording Started');
+});
+
+socket.on( 'data', function ( msg ) {
+  console.log( msg );
+});
+
+socket.on( 'error', function ( error ) {
+  console.log( 'error' + error );
+});
+
+socket.on('close',function(){
+  console.log('服务器端下线了');
+});
+
+const socket2 = new net.Socket();
+const port2 = 4000;
+socket2.setEncoding('UTF-8');
+
+socket2.connect( port2,hostname,function(){
+  socket2.write('Evaluation Started');
+});
+
+socket2.on( 'data', function ( msg ) {
+  console.log( msg );
+});
+
+socket2.on( 'error', function ( error ) {
+  console.log( 'error' + error );
+});
+
+socket2.on('close',function(){
+  console.log('服务器端下线了');
+});
+
+var total_wd = 0;
+var filler_wd = 0;
+filler_set = new Set(["um", "uh","umm","mmm","en", "you know"])
+
 // 得到识别结果后进行处理，仅供参考，具体业务具体对待
 let rtasrResult = []
 ws.on('message', (data, err) => {
   if (err) {
     log.info(`err:${err}`)
+    //slog(`err:${err}`)
     return
   }
   let res = JSON.parse(data)
@@ -52,38 +130,67 @@ ws.on('message', (data, err) => {
     case 'started':
       log.info('started!')
       log.info('sid is:' + res.sid)
-      // 开始读取文件进行传输
-      var readerStream = fs.createReadStream(config.file, {
-        highWaterMark: config.highWaterMark
+
+      // Create path to write recordings to.
+      if (!fs.existsSync(DIRECTORY)){
+        fs.mkdirSync(DIRECTORY);
+      }
+      // Create file path with random name.
+      const fileName = path.join(DIRECTORY, Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 4).concat('.wav'));
+      log.info('Writing new recording file at: ', fileName);
+
+      // Create write stream.
+      
+      const fileStream = fs.createWriteStream(fileName, { encoding: 'binary' });
+      // Start and write to the file.
+      audioRecorder.start().stream().pipe(fileStream);
+      //audioRecorder.start()
+      // Listen to events.
+      audioRecorder.stream().on('close', function(code) {
+  		log.info('Recording closed. Exit code: ', code);
       });
-      readerStream.on('data', function (chunk) {
+      audioRecorder.stream().on('error', function(error) {
+		log.info('Error msg: ${error}')
+      });
+      audioRecorder.stream().on('end', function() {
+		log.info('Audio stream closed');
+		
+		// Stop audio recorder.
+		//audioRecorder.stop();
+	// Exit the program, perhaps here you want to re-enable the hotword detector again.
+	//process.exit(1);
+	});
+      audioRecorder.stream().on('data', function (chunk) {
+	    //log.info('here are a chunk')
         ws.send(chunk)
-      });
-      readerStream.on('end', function () {
-        // 最终帧发送结束
-        ws.send("{\"end\": true}")
       });
       break
     case 'result':
       // ... do something
       let data = JSON.parse(res.data)
       rtasrResult[data.seg_id] = data
-	  // 把转写结果解析为句子
-      if (data.cn.st.type == 0) {
-        rtasrResult.forEach(i => {
-          let str = "实时转写"
-          str += (i.cn.st.type == 0) ? "【最终】识别结果：" : "【中间】识别结果："
-          i.cn.st.rt.forEach(j => {
+	  //only print final recog. result
+
+	  if (data.cn.st.type == 0) {
+		let str= ""
+		data.cn.st.rt.forEach(j => {
             j.ws.forEach(k => {
               k.cw.forEach(l => {
                 str += l.w
               })
             })
           })
-          log.info(str)
-        })
+		socket.write(str)
+		total_wd += str.split(" ").length
+		for (var x of filler_set){
+			filler_wd += get_filler_num(str.toLowerCase(),x)
+		}	
+		socket2.write("Total Word #:"+total_wd+'\n'+"Total Filler #:"+filler_wd)
+	    log.info(str)
+	  }	  
+	  
 
-      }
+
       break
   }
 })
@@ -104,4 +211,13 @@ function getSigna(ts) {
   let sha1 = CryptoJS.HmacSHA1(md5, config.apiKey)
   let base64 = CryptoJS.enc.Base64.stringify(sha1)
   return encodeURIComponent(base64)
+}
+function get_filler_num(one_str, filler){
+	let count =0;
+	let index = one_str.indexOf(filler);
+	while (index !== -1){
+		count++;
+		index = one_str.indexOf(filler, index+1);
+	}
+	return count
 }
